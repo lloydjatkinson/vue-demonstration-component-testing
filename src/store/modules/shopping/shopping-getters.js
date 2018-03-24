360a1566f552797ac33a747c814e23a1905e5c20
Lloyd Atkinson <lloydjatkinson@gmail.com>
2018-03-24 21:51:31 +0000

    Add clearer name to reduce arguments

--- src/store/modules/shopping/shopping-getters.js
+++ src/store/modules/shopping/shopping-getters.js
@@ -2,12 +2,12 @@ const getters = {
     lowStockItems: state => state.availableProducts.filter(product => product.inStock > 10),
     basketItemCount: state => state.basketItems.length,
     basketQuantityTotal: state =>
-        state.basketItems.reduce((accumulator, currentValue) => {
-            return accumulator + currentValue.quantity;
+        state.basketItems.reduce((accumulator, currentProduct) => {
+            return accumulator + currentProduct.quantity;
         }, 0),
     basketPriceTotal: state =>
-        state.basketItems.reduce((accumulator, currentValue) => {
-            return accumulator + state.availableProducts.find(product => product.id === currentValue.id).price * currentValue.quantity;
+        state.basketItems.reduce((accumulator, currentProduct) => {
+            return accumulator + state.availableProducts.find(product => product.id === currentProduct.id).price * currentProduct.quantity;
         }, 0)
 };
 
