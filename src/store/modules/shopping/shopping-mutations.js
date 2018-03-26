4722efbe3afb80042b6079930a4bf2d9e5c79852
Lloyd Atkinson <lloydjatkinson@gmail.com>
2018-03-26 23:17:43 +0100

    Continue implementation state management

--- src/store/modules/shopping/shopping-mutations.js
+++ src/store/modules/shopping/shopping-mutations.js
@@ -22,23 +22,37 @@ const mutations = {
     },
 
     [REMOVE_PRODUCT_FROM_BASKET] (state, id, quantityToRemove) {
-        const product = state.basketItems.find(product => product.id === id);
+        const product = state.basketItems.find(p => p.id === id);
         
         if (product) {
             product.quantity -= quantityToRemove;
-        }
 
-        if (product.quantity >= 0) {
-            // remove with splice
+            if (product.quantity >= 0) {
+                state.basketItems = state.basketItems.filter(p => p.id !== product.id);
+            }
         }
     },
 
     [ADD_PRODUCT_TO_STOCK] (state, id, quantityToAdd) {
+        const product = state.basketItems.find(p => p.id === id);
 
+        if (product) {
+            product.quantity += quantityToAdd;
+        }
     },
 
     [REMOVE_PRODUCT_FROM_STOCK] (state, id, quantityToRemove) {
+        const product = state.availableProducts.find(p => p.id === id);
 
+        // if (product) {
+        //     product.inStock = clamp(product.inStock, 0, )
+
+        //     product.quantity -= quantityToRemove;
+
+        //     if (product) {
+        //         product.quantity -= quantityToRemove;
+        //     }
+        // }
     },
 
     [EMPTY_BASKET] (state) {
